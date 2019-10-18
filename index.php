ef92295e6134291eb4dddb71c85e3dc996b0f358
Bongani NKOSI <bnkosi@c5r4s5.wethinkcode.co.za>
2019-10-18 12:11:52 +0200

    incons header

---
+++ index.php
@@ -0,0 +1,17 @@
+<html>
+    <head>
+        <link rel="stylesheet" href="./css/main.css">
+        <link rel="stylesheet" href="./css/main.css">
+        <title>instaclone</title>
+        <script src="https://kit.fontawesome.com/fcfc638980.js" crossorigin="anonymous"></script>
+    </head>
+    <body>
+        <header>
+            <a href= "home.html"><i class="fas fa-home"></i></a>
+            <a href= "search.html"><i class="fas fa-search"></i></a>
+            <a href= "likes.html"><i class="fas fa-heart"></i></a>
+            <a href= "profile.html"><i class="fas fa-user-circle"></i></a>
+        </header>
+        test
+    </body>
+</html>
