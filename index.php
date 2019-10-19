01ab50f560bde2a0747fb43c1a921a5a00e0bc55
Bongani NKOSI <bnkosi@c5r4s5.wethinkcode.co.za>
2019-10-19 16:27:14 +0200

    login link fail -  rebuilding db

--- index.php
+++ index.php
@@ -3,6 +3,8 @@
 <head>
     <link rel="stylesheet" href="./css/main.css">
     <meta charset="UTF-8">
+    <link rel = "icon" href="./img/logowhite.png" 
+        type = "image/x-icon"> 
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Instaclone</title>
@@ -11,38 +13,35 @@
     <div class="container">
         <div class="frame">
             <div class="logobox">
-            <img src="./img/logo.png">
+                <img src="./img/logo.png">
             </div>
             <h2>Login</h2>
-        <form action="/action_page.php" method="post">
-            <div class="container2">
-            <label for="uname"><b>Username</b></label>
-            <input type="text" placeholder="Enter Username" name="uname" required>
-                <label for="psw"><b>Password</b></label>
+            <form action="/action_page.php" method="post">
+                <div class="container2">
+                  <label for="uname"><b>Username</b></label>
+                  <input type="text" placeholder="Enter Username" name="uname" required>
+                  <label for="psw"><b>Password</b></label>
                   <input type="password" placeholder="Enter Password" name="psw" required>     
                   <button type="submit">Login</button>
-                <label>
-                    <input type="checkbox" checked="checked" name="remember"> Remember me
-                </label>
-    </div>
-
-          <div class="container3">
-              <button type="button" class="cancelbtn">Cancel</button>
-              <span class="psw">Forgot <a href="#">password?</a></span>
-          </div>
-        </form>
+                  <label><input type="checkbox" checked="checked" name="remember"> Remember me</label>
+                </div>
+                <div class="container3">
+                    <button type="button" class="cancelbtn">Cancel</button>
+                    <span class="psw">Forgot <a href="#">password?</a></span>
+                </div>
+            </form>
         </div>
     </div>
     <div class="waveWrapper waveAnimation">
-  <div class="waveWrapperInner bgTop">
-    <div class="wave waveTop" style="background-image: url('http://front-end-noobs.com/jecko/img/wave-top.png')"></div>
-  </div>
-  <div class="waveWrapperInner bgMiddle">
-    <div class="wave waveMiddle" style="background-image: url('http://front-end-noobs.com/jecko/img/wave-mid.png')"></div>
-  </div>
-  <div class="waveWrapperInner bgBottom">
-    <div class="wave waveBottom" style="background-image: url('http://front-end-noobs.com/jecko/img/wave-bot.png')"></div>
-  </div>
+          <div class="waveWrapperInner bgTop">
+            <div class="wave waveTop" style="background-image: url('http://front-end-noobs.com/jecko/img/wave-top.png')"></div>
+        </div>
+        <div class="waveWrapperInner bgMiddle">
+          <div class="wave waveMiddle" style="background-image: url('http://front-end-noobs.com/jecko/img/wave-mid.png')"></div>
+        </div>
+        <div class="waveWrapperInner bgBottom">
+            <div class="wave waveBottom" style="background-image: url('http://front-end-noobs.com/jecko/img/wave-bot.png')"></div>
+    </div>
 </div>
 </body>
 </html>
