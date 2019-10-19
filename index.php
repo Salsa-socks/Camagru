6fd8244b73ee07a6e7ae63265527016ae38c56c8
Bongani NKOSI <bnkosi@c4r21s5.wethinkcode.co.za>
2019-10-19 13:27:53 +0200

    loginform

--- index.php
+++ index.php
@@ -1,27 +1,39 @@
-<html>
-    <head>
-        <link rel="stylesheet" href="./css/main.css">
-        <link rel="stylesheet" href="./css/main.css">
-        <title>instaclone</title>
-        <script src="https://kit.fontawesome.com/fcfc638980.js" crossorigin="anonymous"></script>
-    </head>
-    <body>
-        <header>
-            <a href= "home.html"><i class="fas fa-home"></i></a>
-            <a href= "search.html"><i class="fas fa-search"></i></a>
-            <a href= "likes.html"><i class="fas fa-heart"></i></a>
-            <a href= "profile.html"><i class="fas fa-user-circle"></i></a>
-        </header>
-
-        <div class="container">
-            <div class="frame">
-                <div class="profile">
-                    <div class="usern">
-                    </div>
-                    <div class="location">
-                    </div>
-                </div>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <link rel="stylesheet" href="./css/main.css">
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Instaclone</title>
+</head>
+<body>
+    <div class="container">
+        <div class="frame">
+            <div class="logobox">
+            <img src="./img/logo.png">
             </div>
+            <h2>Login</h2>
+            <form action="/action_page.php" method="post">
+            <div class="container2">
+    <label for="uname"><b>Username</b></label>
+    <input type="text" placeholder="Enter Username" name="uname" required>
+
+    <label for="psw"><b>Password</b></label>
+    <input type="password" placeholder="Enter Password" name="psw" required>
+        
+    <button type="submit">Login</button>
+    <label>
+      <input type="checkbox" checked="checked" name="remember"> Remember me
+    </label>
+  </div>
+
+  <div class="container3" style="background-color:#f1f1f1">
+    <button type="button" class="cancelbtn">Cancel</button>
+    <span class="psw">Forgot <a href="#">password?</a></span>
+  </div>
+</form>
         </div>
-    </body>
+    </div>
+</body>
 </html>
---
+++ user.php
@@ -0,0 +1,31 @@
+<html>
+    <head>
+        <link rel="stylesheet" href="./css/main.css">
+        <title>instaclone</title>
+        <script src="https://kit.fontawesome.com/fcfc638980.js" crossorigin="anonymous"></script>
+        <link href="https://fonts.googleapis.com/css?family=Oswald:400,600,700&display=swap" rel="stylesheet">
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <meta http-equiv="X-UA-Compatible" content="ie=edge">
+        <title>Instaclone</title>
+    </head>
+    <body>
+        <header>
+            <a href= "home.html"><i class="fas fa-home"></i></a>
+            <a href= "search.html"><i class="fas fa-search"></i></a>
+            <a href= "likes.html"><i class="fas fa-heart"></i></a>
+            <a href= "profile.html"><i class="fas fa-user-circle"></i></a>
+        </header>
+        <div class="container">
+            <div class="frame">
+                <div class="profile">
+                    <div class="circle"></div>
+                        <div class="user-d">
+                        <div class="usern"><p>Food</p></div>
+                        <div class="location"><p>Cheese</p></div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </body>
+</html>
