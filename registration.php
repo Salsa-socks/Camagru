eef19d0792ed748ac7d8ad7e6075df68f9d8d5b0
Bongani NKOSI <bnkosi@c5r4s5.wethinkcode.co.za>
2019-10-20 12:12:02 +0200

    tequila

--- registration.php
+++ registration.php
@@ -18,20 +18,20 @@
     <p>Please fill in this form to create an account.</p>
     <hr>
 
-    <label for="name"><b>Name</b></label>
-    <input type="text" placeholder="Enter your name" name="name" required>
+        <label for="name"><b>Name</b></label>
+        <input type="text" placeholder="Enter your name" name="name" required>
 
-    <label for="username"><b>Username</b></label>
-    <input type="text" placeholder="Enter desired username" name="username" required>
+        <label for="username"><b>Username</b></label>
+        <input type="text" placeholder="Enter desired username" name="username" required>
 
-    <label for="email"><b>Email</b></label>
-    <input type="text" placeholder="Enter Email" name="email" required>
+        <label for="email"><b>Email</b></label>
+        <input type="text" placeholder="Enter Email" name="email" required>
 
-    <label for="psw"><b>Password</b></label>
-    <input type="password" placeholder="Enter Password" name="psw" required>
+        <label for="psw"><b>Password</b></label>
+        <input type="password" placeholder="Enter Password" name="psw" required>
 
-    <label for="psw-repeat"><b>Repeat Password</b></label>
-    <input type="password" placeholder="Repeat Password" name="psw-repeat" required>
+        <label for="psw-repeat"><b>Repeat Password</b></label>
+        <input type="password" placeholder="Repeat Password" name="psw-repeat" required>
     <hr>
     <p>By creating an account you agree to our <a href="#">Terms & Privacy</a>.</p>
 
