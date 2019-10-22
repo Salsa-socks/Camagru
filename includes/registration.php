91f4a73fce5e74710c6a904248f872076d548909
Bongani NKOSI <bnkosi@c5r4s5.wethinkcode.co.za>
2019-10-22 16:35:40 +0200

    configuring website to MVC

--- includes/registration.php
+++ includes/registration.php
@@ -6,13 +6,13 @@
     <link rel = "icon" href="./img/logowhite.png" type = "image/x-icon"> 
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>registration</title>
+    <title>Registration</title>
 </head>
 <body>
     <div class="logobox">
         <img src="./img/logo.png">
     </div>
-<form action="/action_page.php">
+<form action="registration.php">
   <div class="container">
     <h1>Register</h1>
     <p>Please fill in this form to create an account.</p>
@@ -30,8 +30,8 @@
         <label for="psw"><b>Password</b></label>
         <input type="password" placeholder="Enter Password" name="psw" required>
 
-        <label for="psw-repeat"><b>Repeat Password</b></label>
-        <input type="password" placeholder="Repeat Password" name="psw-repeat" required>
+        <label for="psw_repeat"><b>Repeat Password</b></label>
+        <input type="password" placeholder="Repeat Password" name="psw_repeat" required>
     <hr>
     <p>By creating an account you agree to our <a href="#">Terms & Privacy</a>.</p>
 
@@ -39,7 +39,7 @@
   </div>
   
   <div class="container signin">
-    <p>Already have an account? <a href="#">Sign in</a>.</p>
+    <p>Already have an account? <a href="index.php">Sign in</a>.</p>
   </div>
 </form>
 </body>
