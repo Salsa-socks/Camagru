ed532c769c86271175ff55caaa102526042724c9
Bongani NKOSI <bnkosi@c5r4s5.wethinkcode.co.za>
2019-11-07 15:47:19 +0200

    fixing forgot password

--- includes/upload.php
+++ includes/upload.php
@@ -1,25 +1,20 @@
 <?php
    require_once '../core/init.php';
 
-   echo "WTF";
    $user = new User();
    $username= $user->data()->username;
    if (!$user->isLoggedIn()) {
     Redirect::to('login.php');
     }
-   $baseimage = $_POST["image"];
-   echo $baseimage;
+   $layer1 = $_POST["baseimage"];
 
-   if(!empty($layer1) && !empty($usern)){
-    $baseimage = $usern.time().".png";
-    $imagepath = "../../gallery/photos/".$baseimage;
+   if(!empty($layer1) && !empty($username)){
+    $baseimage = $username.time().".png";
+    $imagepath = "./usergallery/".$baseimage;
     $imgurl = str_replace("data:image/png;base64,", "", $layer1);
     $imageurl = str_replace(" ", "+", $imgurl);
     $imgdecode = base64_decode($imageurl);
     file_put_contents($imagepath, $imgdecode);
    }
-
-   
-
 
 ?>
